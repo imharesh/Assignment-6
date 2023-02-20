7d00ddf15dd66f85db13e5d2ea85c8ff40bbb71a
imharesh <hareshbaraiya2286@gmail.com>
2023-02-20 17:48:04 +0530

    Add Assignment 6

---
+++ Assignment6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+class Program
+{
+    // main method - Asynchronous
+    static async Task Main(string[] args)
+    {
+       // input from user 
+         Console.Write("Enter URL : ");
+         string url = Console.ReadLine();
+
+        // create the  http request and response
+        using (HttpClient client = new HttpClient())
+          { 
+            // using GetAsync - for get request 
+            HttpResponseMessage response = await client.GetAsync(url);
+            // using ReadAsStringAsync - read the content
+            string responseText = await response.Content.ReadAsStringAsync();
+
+         
+            Console.WriteLine("Downloaded and saved to A.txt.");
+            // write the data in file 
+            File.WriteAllText("A.txt", responseText);
+            
+          }
+    }
+}
+
